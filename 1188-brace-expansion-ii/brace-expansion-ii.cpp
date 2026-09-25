022e55a7319f935eb30347c2f5d8379fe8ffef2c
aryantiwari1118 <110696270+aryantiwari1118@users.noreply.github.com>
2026-09-25 07:15:46 +0530

    Time: 0 ms (100.00%) | Memory: 13.3 MB (97.77%) - LeetSync

---
+++ 1188-brace-expansion-ii/brace-expansion-ii.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<char> op;
+    vector<vector<string>> st;
+
+    void perf() {
+        int x=st.size()-2, y = st.size()-1;
+        if (op.back()=='+'){
+            st[x].reserve(st[x].size()+st[y].size());
+            st[x].insert(st[x].end(), st[y].begin(), st[y].end());
+        }
+        else { 
+            vector<string> nxt;
+            nxt.reserve(st[x].size()*st[y].size());
+            for (const auto& l : st[x]) {
+                for (const auto& r : st[y]) 
+                    nxt.push_back(l+r);
+            }
+            st[x]=move(nxt);
+        }
+        op.pop_back();
+        st.pop_back();
+    }
+    vector<string> braceExpansionII(string& expr) {
+        const int n=expr.size();
+        op.reserve(n);
+
+        char prv='@', cur;
+
+        for (int i=0; i<n; i++, prv=cur) {
+            cur = expr[i];
+            switch (cur) {
+            case ',': 
+                while (!op.empty() && op.back() != '{') perf();
+                op.push_back('+');
+                break;
+            case '{':
+                if (prv=='}' || isalpha(prv)) op.push_back('*');
+                op.push_back('{');
+                break;
+            case '}':
+                while (!op.empty() && op.back()!='{') perf();
+                op.pop_back();
+                break;
+            default:
+                if (prv=='}') op.push_back('*');
+                string s;
+                for (; i<n && isalpha(expr[i]); i++) s+=expr[i];
+                st.push_back({s});
+                i--;
+                cur=expr[i];
+            }
+        }
+
+        while (!op.empty()) perf();
+
+        auto ans=st.front();
+        sort(ans.begin(), ans.end());
+        ans.erase(unique(ans.begin(), ans.end()), ans.end());
+        return ans;
+    }
+};
+
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
